c0a92997fdece63b6c5217f63060f6aa0898ddb1
Rogick Manoel <rmanoel@inca.gov.br>
2020-09-29 12:48:26 -0300

    Ajustes no .travis.yml

---
+++ src/main/java/com/rgk/ponto/api/entity/Empresa.java
@@ -0,0 +1,5 @@
+package com.rgk.ponto.api.entity;
+
+public class Empresa {
+
+}
---
+++ src/main/java/com/rgk/ponto/api/enumeration/PerfilEnum.java
@@ -0,0 +1,8 @@
+package com.rgk.ponto.api.enumeration;
+
+public enum PerfilEnum {
+	ROLE_ADMIN,
+	ROLE_FUNC
+
+	
+}
